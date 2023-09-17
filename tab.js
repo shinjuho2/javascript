5df68a5d3d9590f05b7f86f21f793b414f00bed4
shinjuho2 <sinjuho2@gmail.com>
2023-09-17 23:54:27 +0900

    4일차 for문으로

--- tab.js
+++ tab.js
@@ -1,20 +1,10 @@
-$('.tab-button').eq(0).on('click', function() {
-    $('.tab-button').removeClass('orange')
-    $('.tab-button').eq(0).addClass('orange')
-    $('.tab-content').removeClass('show')
-    $('.tab-content').eq(0).addClass('show')
-});
+for (let i = 0; i < 3; i++){
 
-$('.tab-button').eq(1).on('click', function() {
-    $('.tab-button').removeClass('orange')
-    $('.tab-button').eq(1).addClass('orange')
-    $('.tab-content').removeClass('show')
-    $('.tab-content').eq(1).addClass('show')
-});
-
-$('.tab-button').eq(2).on('click', function() {
-    $('.tab-button').removeClass('orange')
-    $('.tab-button').eq(2).addClass('orange')
-    $('.tab-content').removeClass('show')
-    $('.tab-content').eq(2).addClass('show')
-});
+    $('.tab-button').eq(i).on('click', function() {
+      $('.tab-button').removeClass('orange');
+      $('.tab-button').eq(i).addClass('orange');
+      $('.tab-content').removeClass('show');
+      $('.tab-content').eq(i).addClass('show');
+    })
+  
+  };
