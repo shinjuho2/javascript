44d721d31d8d1bc3efc794d1a987cb4ba686bc41
shinjuho2 <sinjuho2@gmail.com>
2023-09-17 23:40:12 +0900

    4일차 탭기능

---
+++ tab.js
@@ -0,0 +1,29 @@
+$('.tab-button').eq(0).on('click', function() {
+    $('.tab-button').removeClass('orange')
+    $('.tab-button').eq(0).addClass('orange')
+});
+
+$('.tab-button').eq(0).on('click', function() {
+    $('.tab-content').removeClass('show')
+    $('.tab-content').eq(0).addClass('show')
+});
+
+$('.tab-button').eq(1).on('click', function() {
+    $('.tab-button').removeClass('orange')
+    $('.tab-button').eq(1).addClass('orange')
+});
+
+$('.tab-button').eq(1).on('click', function() {
+    $('.tab-content').removeClass('show')
+    $('.tab-content').eq(1).addClass('show')
+});
+
+$('.tab-button').eq(2).on('click', function() {
+    $('.tab-button').removeClass('orange')
+    $('.tab-button').eq(2).addClass('orange')
+});
+
+$('.tab-button').eq(2).on('click', function() {
+    $('.tab-content').removeClass('show')
+    $('.tab-content').eq(2).addClass('show')
+});
