1b3702a64fa0d442e74090288b6759dc79dc3fa5
shinjuho2 <sinjuho2@gmail.com>
2023-09-18 21:18:27 +0900

    5일차 dataset

--- tab.js
+++ tab.js
@@ -1,10 +1,21 @@
-for (let i = 0; i < 3; i++){
+/*for (let i = 0; i < 3; i++){
 
     $('.tab-button').eq(i).on('click', function() {
-      $('.tab-button').removeClass('orange');
-      $('.tab-button').eq(i).addClass('orange');
-      $('.tab-content').removeClass('show');
-      $('.tab-content').eq(i).addClass('show');
+      tab(i)
     })
   
   };
+*/
+
+$('.list').click(function(e) {
+
+    tab(e.target.dataset.id)
+
+})
+
+  function tab(num) {
+      $('.tab-button').removeClass('orange');
+      $('.tab-button').eq(num).addClass('orange');
+      $('.tab-content').removeClass('show');
+      $('.tab-content').eq(num).addClass('show');
+  }
